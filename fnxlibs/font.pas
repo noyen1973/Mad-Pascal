0fd44a3671b47432f3e6016104b1beb07542e97b
Norman Yen <52337445+noyen1973@users.noreply.github.com>
2024-02-10 10:34:52 -0800

    Update font.pas

--- fnxlibs/font.pas
+++ fnxlibs/font.pas
@@ -7,67 +7,16 @@ interface
 
 uses f256;
 
-procedure Init(idx: byte; addr: word); register; assembler;
+procedure Init(idx: byte; addr: word);
 
 
 // ------------------------------------
 implementation
 // ------------------------------------
 
-procedure Init(idx: byte; addr: word); register; assembler;
-// var
-//     fnt: ^byte absolute $C000;
-//     src: ^byte;
-//     i: word;
-// begin
-//     IOPAGE_CTRL := iopPage1;
-
-//     src := pointer(addr);
-
-//     if idx = 1 then
-//         fnt := fnt + $800;
-
-//     for i := 0 to 1023 do begin
-//         fnt^ := src^;
-//         inc(fnt);
-//         inc(src);
-//     end;
-// end;
-asm
-        phx
-        phy
-
-        ldy IOPAGE_CTRL
-        phy
-
-        ldx #@iopagectrl(iopPage1)
-        stx IOPAGE_CTRL
-
-        lda :edx
-        asl
-        asl
-        asl
-        ora #>FONT_MEMORY_BANK0
-        sta :edx+1
-        lda #<FONT_MEMORY_BANK0
-        sta :edx
-        ldx #8
-        ldy #0
-_1
-        lda (:ecx),y
-        sta (:edx),y
-        iny
-        bne _1
-        inc :ecx+1
-        inc :edx+1
-        dex
-        bne _1
-
-        ply
-        sty IOPAGE_CTRL
-
-        ply
-        plx
+procedure Init(idx: byte; addr: word);
+begin
+    move(pointer(addr),pointer(idx*$0800+FONT_MEMORY_BANK0),$0800)
 end;
 
 end.
